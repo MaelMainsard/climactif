95f6a521ed5139e451c65d642c3f9036d756a956
maelmainsard <mainsardm@gmail.com>
2025-06-30 11:46:01 +0200

    fix(cicd): Correction audit lighthouse

--- accessibility/lighthouse-ci/.lighthouserc.js
+++ accessibility/lighthouse-ci/.lighthouserc.js
@@ -2,18 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist/climactif/browser',
-      url: ['http://localhost:3000/'],
-      // numberOfRuns: 3,
-      // settings: {
-      //   chromeFlags: [
-      //     '--headless',
-      //     '--disable-gpu',
-      //     '--no-sandbox',
-      //     '--disable-dev-shm-usage',
-      //     '--disable-setuid-sandbox',
-      //     '--disable-web-security'
-      //   ]
-      // }
+      url: ['http://localhost:3000/home'],
     },
     assets: {
       assertions: {

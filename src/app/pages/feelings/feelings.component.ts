9c762fa93ab12f5eb3eecf9841c022beed7c4d1e
Maxence.p <maxence.pille@kandixi.com>
2025-06-30 14:04:40 +0200

    feat(refresh-title) add component

--- src/app/pages/feelings/feelings.component.ts
+++ src/app/pages/feelings/feelings.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RefreshTitleComponent } from '../../components/refresh-title/refresh-title.component';
 
 @Component({
   selector: 'app-feelings',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RefreshTitleComponent],
   templateUrl: './feelings.component.html',
 })
 export class FeelingsComponent {}

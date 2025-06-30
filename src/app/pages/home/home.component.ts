88cdeac875f7d3c03c3776853ca990a3078b1362
Maxence.p <maxence.pille@kandixi.com>
2025-06-30 11:29:38 +0200

    navbar work

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 @Component({
   selector: 'app-home',
   imports: [NavbarComponent],
+  standalone: true,
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}

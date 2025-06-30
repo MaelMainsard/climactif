d546fed89870dbfdf8c5a975a3fa18c4f280839b
SymAir <symeon@oclocher.fr>
2025-06-30 16:15:59 +0200

    First shot of form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dialog } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
+import { InputTextarea } from 'primeng/inputtextarea';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SliderModule } from 'primeng/slider';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+interface Keyword {
+  name: string;
+  code: string;
+}
 
 @Component({
-  selector: 'dialog-basic-demo',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  imports: [Dialog, ButtonModule, InputTextModule],
+  imports: [
+    Dialog,
+    ButtonModule,
+    InputTextarea,
+    MultiSelectModule,
+    SliderModule,
+    ReactiveFormsModule,
+  ],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   visible: boolean = false;
+  moodForm!: FormGroup;
+  keywords: Keyword[] = [];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.moodForm = this.fb.group({
+      description: [''],
+      keywords: [[]],
+      color: [50],
+    });
+
+    this.keywords = [
+      { name: 'Happy', code: 'HP' },
+      { name: 'Sad', code: 'S' },
+      { name: 'Angry', code: 'A' },
+      { name: 'Excited', code: 'E' },
+      { name: 'Calm', code: 'C' },
+    ];
+  }
 
   showDialog() {
     this.visible = true;
+  }
+
+  saveMood() {
+    console.log(this.moodForm.value);
+    this.visible = false;
   }
 }

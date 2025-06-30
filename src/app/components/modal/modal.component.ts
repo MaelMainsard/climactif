d2501851650344df1cfb0ed873dba6e7e70412e2
SymAir <symeon@oclocher.fr>
2025-06-30 11:30:19 +0200

    Animating modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-modal',
@@ -7,6 +14,23 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css',
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate(
+          '300ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)' }),
+        ),
+      ]),
+      transition('* => void', [
+        animate(
+          '300ms ease-in',
+          style({ opacity: 0, transform: 'translateY(20px)' }),
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ModalComponent {
   @Input() isOpen = false;

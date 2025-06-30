59bbed4a728d59afbf2a28e597ce758dffef9ef7
maelmainsard <mainsardm@gmail.com>
2025-06-30 11:22:03 +0200

    fix(cicd): Test avec lighthouse en job

--- accessibility/lighthouse-ci/.lighthouserc.js
+++ accessibility/lighthouse-ci/.lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './dist/climactif/browser',
-      // url: ['http://localhost:3000/'],
+      url: ['http://localhost:3000/'],
       // numberOfRuns: 3,
       // settings: {
       //   chromeFlags: [

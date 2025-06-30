1f430556e4efeda72abdebd52dcaa1525e560169
SymAir <symeon@oclocher.fr>
2025-06-30 11:41:16 +0200

    Added mood form component

---
+++ src/app/components/mood-form/mood-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mood-form',
+  imports: [],
+  templateUrl: './mood-form.component.html',
+  styleUrl: './mood-form.component.css'
+})
+export class MoodFormComponent {
+
+}

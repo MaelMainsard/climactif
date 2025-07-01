f4b6c5dd2847d1b34373eb09b096b390c60c00fd
maelmainsard <mainsardm@gmail.com>
2025-07-01 13:21:47 +0200

    fix(navbar): Ajout d'un darmode switch dans la nav bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MenuItem } from 'primeng/api';
 import { Menubar } from 'primeng/menubar';
+import {ToggleSwitch} from 'primeng/toggleswitch';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ButtonModule,RouterModule, Menubar],
+  imports: [ButtonModule, RouterModule, Menubar, ToggleSwitch, FormsModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  darkMode: boolean = false;
+
   items: MenuItem[] = [
     {
       label: 'Accueil',
@@ -24,7 +28,7 @@ export class NavbarComponent {
     },
   ];
 
-  toggleDarkMode() {
+  toggleDarkMode()  {
     const element = document.querySelector('html');
     if (element != null)
       element.classList.toggle('my-app-dark');

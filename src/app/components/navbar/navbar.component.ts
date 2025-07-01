ceb72138ec052a0c44e126b8f5dabfca2a7e8e8c
Maxence.p <maxence.pille@kandixi.com>
2025-07-01 08:59:44 +0200

    fix(navbar) use menubar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, Menubar],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent implements OnInit {
+  items: MenuItem[] = [
+    {
+      label: 'Accueil',
+      icon: 'pi pi-home',
+      routerLink: '/home',
+    },
+    {
+      label: 'Sentiments',
+      icon: 'pi pi-heart',
+      routerLink: '/feelings',
+    },
+  ];
+
   ngOnInit() {
     console.log('Navbar coucou');
   }

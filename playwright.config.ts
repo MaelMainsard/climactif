52621c146dc1b3932ad55242599df88dcbe0af68
maelmainsard <mainsardm@gmail.com>
2025-07-07 11:31:17 +0200

    fix(playwright): Correction de la ci playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   reporter: [
     ['@skilbourn/playwright-report-summary', { outputFile: 'playwright-summary.txt' }],
-    ['json', {  outputFile: 'playwright-results.json' }],
     ['html']
   ],
   projects: [
